fd39bf797c78fedd9310d082f5271fc4298f5cd1
Isaac Sarte <aizakku09@gmail.com>
2022-05-01 23:24:30 +0800

    fixed foreign keys

--- app/Order.php
+++ app/Order.php
@@ -8,10 +8,10 @@ class Order extends Model
 {
     //
     public function customer(){
-        return $this->belongsTo(Customer::class, 'foreign_key');
+        return $this->belongsTo(Customer::class, 'customer_id');
     }
 
     public function product(){
-        return $this->belongsTo(Product::class, 'foreign_key');
+        return $this->belongsTo(Product::class, 'product_id');
     }
 }
